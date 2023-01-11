655f078a602c645b02c90db12df75ab778d7e4dc
Azeem Akram <azeem.4033@gmail.com>
2023-01-11 04:57:06 -0600

    Program to reverse a string
    Program to reverse a string

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter your string");
+            string stringtoreverse=Console.ReadLine();
+            string reversed = null;
+            reversed = funtiontoreverseastring(stringtoreverse);
+
+        }
+
+        private static string funtiontoreverseastring(string stringtoreverse)
+        {
+            string reversedx = null ;
+            int j = 0;
+
+            for (int i = stringtoreverse.Length - 1; i >= 0; i--)
+            {
+                reversedx = reversedx + stringtoreverse[i] ;
+                j++;
+            }
+            return reversedx;
         }
     }
 }
