ec03c36e74381d3c797538d9ab68e1739e72a907
Mujahed032 <mohdmujahed032@gmail.com>
2023-01-14 23:33:40 +0530

    Created string to int progamme
    Created string to int progamme

---
+++ Stringtonumber by using Function/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your string");
+            string userEntername = Console.ReadLine();
+            int age = Recieveage(userEntername);
+            Console.WriteLine(age);
+        }
+
+        private static int Recieveage(string? userEntername)
+        {
+            int returnage = 0;
+            switch(userEntername)
+            {
+                
+                case  "khizar":
+                    returnage =28;
+                    break;
+                case "malik":
+                    returnage = 24;
+                    break;
+                
+            }
+            return  returnage;
+        }
+    }
+}
