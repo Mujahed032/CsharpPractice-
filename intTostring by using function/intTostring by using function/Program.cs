d70f6dcabeaa460ee30c81556e9c66d687ef6e9e
Mujahed032 <mohdmujahed032@gmail.com>
2023-01-15 00:30:28 +0530

    intTostring using function progamme
    intTostring using function progamme

---
+++ intTostring by using function/intTostring by using function/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace intTostring_by_using_function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your age");
+            int age=int.Parse(Console.ReadLine());
+            string name = reversename(age);
+            Console.WriteLine(name);
+        }
+
+        private static string reversename(int age)
+        {
+            string returnnames = null;
+            switch(age)
+             {
+                case 28:
+                    returnnames = "khizar";
+                    break;
+                case 24:
+                    returnnames = "malik";
+                    break;
+
+            }
+            return returnnames;
+        }
+    }
+}
