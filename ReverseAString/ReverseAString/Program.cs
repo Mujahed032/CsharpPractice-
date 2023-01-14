6886518bb8d80adb803cf5dfd0cd8491a03c358e
Azeem Akram <azeem.4033@gmail.com>
2023-01-14 04:05:16 -0600

    Wrote a new program to reverse A String
    Wrote a new program to reverse A String

---
+++ ReverseAString/ReverseAString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your string");
+            string stringtoreverse = Console.ReadLine();
+            string reversed = null;
+            reversed = funtiontoreverseastring(stringtoreverse);
+            Console.WriteLine($"Your reversed string is : {reversed}");
+        }
+
+        private static string funtiontoreverseastring(string stringtoreverse)
+        {
+            string reversedx = null;
+            int j = 0;
+
+            for (int i = stringtoreverse.Length - 1; i >= 0; i--)
+            {
+                reversedx = reversedx + stringtoreverse[i];
+                j++;
+            }
+            return reversedx;
+        }
+    }
+}
