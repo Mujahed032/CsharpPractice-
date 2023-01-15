ec5764571bdb29410f8bae8b2bbb2c7098ca18f2
Mujahed032 <mohdmujahed032@gmail.com>
2023-01-16 02:07:12 +0530

    intTostring by using List and exception
    intTostring by using List and exception

---
+++ intTostring by using List and exception/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter number between 2 to 9");
+                int number = int.Parse(Console.ReadLine());
+                List<char> keypardletters = functionforkeypardletter(number);
+                if (keypardletters.Count == 0)
+                {
+                    foreach (char c in keypardletters)
+                    {
+                        Console.WriteLine(c);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("we encountered a problem please reachout to me");
+                Console.ReadLine();
+            }
+        }
+
+        private static List<char> functionforkeypardletter(int number)
+        {
+            List<char> returnletters= new List<char>();
+            switch(number)
+            {
+                case 2:
+                returnletters.Add('a');
+                    returnletters.Add('b');
+                        returnletters.Add('c');
+                       break;
+            }
+            return returnletters;
+        }
+    }
+}
