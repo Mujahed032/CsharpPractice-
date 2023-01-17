345038c9fede68ccc34a4927a9d6e97685cbba1c
Mujahed032 <mohdmujahed032@gmail.com>
2023-01-17 19:12:21 +0530

    Multi level Inheritence
    Multi level Inheritence

---
+++ Multi level Inheritence/Multi level Inheritence/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Multi_level_Inheritence
+{
+    class Car
+    {
+        internal string model;
+        internal string color;
+        internal int price;
+
+        internal void printCarInfo()
+        {
+            Console.WriteLine("model:" + model);
+            Console.WriteLine("color:" + color);
+            Console.WriteLine("price:" + price);
+        }
+    }
+    class Ferrari : Car
+    {
+        internal int speed;
+
+        internal void printspeed()
+        {
+            Console.WriteLine("speed:" + speed);
+        }
+    }
+    class Maruti : Ferrari
+    {
+        double mileage;
+        internal void Displaymileage()
+        {
+            Console.WriteLine("mileage:" + mileage);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maruti mycar = new Maruti();
+            mycar.model = "Alto";
+            mycar.color = "Red";
+            mycar.price = 20000;
+            mycar.speed = 500;
+            mycar.printCarInfo();
+            mycar.Displaymileage();
+            mycar.printspeed();
+            Console.ReadLine();
+
+        }
+    }
+}
