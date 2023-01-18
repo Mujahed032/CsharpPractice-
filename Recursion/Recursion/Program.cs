90cbb821435fe37e286a91c40a8633bfa1e76f7c
Azeem Akram <mohdmujahed032@gmail.com>
2023-01-18 03:36:07 -0600

    Wrote recursion program
    Wrote recursion program

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number you want to find the factorial for: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int factorial = RecursionFactorial(number);
+            
+            Console.WriteLine("Factorial of the number is " + factorial);   
+        }
+
+        private static int RecursionFactorial(int number)
+        {
+            int numberToReturn = 0;
+            if (number == 2)
+                return 2;
+            if(number == 1)
+                return  1;
+
+            numberToReturn = number * RecursionFactorial(number - 1);
+            return numberToReturn;
+        }
+    }
+}
