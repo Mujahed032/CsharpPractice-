25eecab1ae9ef18e2d3622c0b00abebee3bbf619
Azeem Akram <mohdmujahed032@gmail.com>
2023-01-18 04:25:59 -0600

    Example to read from file
    Example to read from file

---
+++ ReadFromFile/ReadFromFile/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ReadFromFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileContents = File.ReadAllLines("C:\\Windows\\Logs\\CBS\\CbsPersist_20230112171928.log");
+
+            foreach(string eachLine in fileContents)
+                Console.WriteLine(eachLine);
+        }
+    }
+}
