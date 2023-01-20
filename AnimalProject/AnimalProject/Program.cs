47baaa5c51d24cd947ace7085d4aa3c1fa57bf5d
Mujahed032 <mohdmujahed032@gmail.com>
2023-01-20 21:45:47 +0530

    FlyingAnimal added
    FlyingAnimal added

--- AnimalProject/AnimalProject/Program.cs
+++ AnimalProject/AnimalProject/Program.cs
@@ -10,6 +10,10 @@
             Horse horseAnimal = new Horse("Horse", false);
             horseAnimal.MakesSound();
 
+            pigeon pigeonAnimal = new pigeon("pigeon", true);
+            pigeonAnimal.MakesSound();
+
+
             //string soundOfHorse = horseAnimal.MakesSound();
 
             //Console.WriteLine($"The sound a horse makes is {soundOfHorse}");
@@ -18,13 +22,16 @@
             Animal thisAnimal;
 
             thisAnimal = horseAnimal;
-
-                thisAnimal.DescribeMe();
+            thisAnimal.DescribeMe();
          
 
             thisAnimal = catAnimal;
-            
-           thisAnimal.DescribeMe();
+            thisAnimal.DescribeMe();
+
+            thisAnimal = pigeonAnimal;
+            thisAnimal.DescribeMe();
+
+
             Console.ReadLine();
 
         }
---
+++ AnimalProject/AnimalProject/pigeon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalProject
+{
+    internal class pigeon : Animal
+    { 
+            public pigeon(string Name, bool canFly)
+            {
+                this.Name = Name;
+                this.CanFly = canFly;
+            }
+            public override string MakesSound()
+            {
+                return "coo";
+            }
+        
+    }
+}
