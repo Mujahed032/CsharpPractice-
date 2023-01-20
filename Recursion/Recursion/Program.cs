42ad583872d76fe6d00eef2b38c69ba91424cc9c
Azeem Akram <mohdmujahed032@gmail.com>
2023-01-20 04:30:02 -0600

    Recursion preventing StackOverFlow
    Runtime StackOverFlowException cannot be handled by the code. It is always thrown to the user. We need to stop the code from going into infinite loop.

--- Recursion/Recursion/Program.cs
+++ Recursion/Recursion/Program.cs
@@ -4,23 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number you want to find the factorial for: ");
-            int number = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Enter a number you want to find the factorial for: ");
+                int number = int.Parse(Console.ReadLine());
+                if(number <= 0)
+                {
+                    Console.WriteLine("This input cannot be accepted. It should be greater than zero.");
+                    return;
+                }
+                int factorial = RecursionFactorial(number);
 
-            int factorial = RecursionFactorial(number);
+                Console.WriteLine("Factorial of the number is " + factorial);
+            }
             
-            Console.WriteLine("Factorial of the number is " + factorial);   
+            catch (Exception ex1)
+            {
+                Console.WriteLine("The process was terminated prematurely. See the logs.");
+            }
+            
+           
+            
         }
 
         private static int RecursionFactorial(int number)
         {
+            
             int numberToReturn = 0;
             if (number == 2)
                 return 2;
             if(number == 1)
                 return  1;
 
-            numberToReturn = number * RecursionFactorial(number - 1);
+            numberToReturn = number + RecursionFactorial(number - 1);
             return numberToReturn;
         }
     }
