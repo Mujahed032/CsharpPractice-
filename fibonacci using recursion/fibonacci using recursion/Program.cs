0f89aae105bd4cf1f081294cec7d3e91d39c2da3
Mujahed032 <mohdmujahed032@gmail.com>
2023-01-25 18:32:59 +0530

    fibonacci using recursion
    fibonacci using recursion

---
+++ fibonacci using recursion/fibonacci using recursion/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace fibonacci_using_recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int number = int.Parse(Console.ReadLine());
+
+            int fibonacci = Functionoffibonaaci(number);
+
+            Console.WriteLine(fibonacci);
+            Console.ReadLine();
+        }
+
+        private static int Functionoffibonaaci(int number)
+        {
+            int returnnumber = 0;
+            if (number == 2)
+                return 2;
+            if(number ==1)
+                return 1;
+
+            returnnumber = Functionoffibonaaci(number - 2) + Functionoffibonaaci(number - 1);
+            return returnnumber;
+
+        }
+    }
+}
