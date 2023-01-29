781fe67afe000d58d92698a1cef25263f77da701
Mujahed032 <mohdmujahed032@gmail.com>
2023-01-29 23:42:27 +0530

    Inheritance in Author class.
    Inheritance in Author class.

--- Inheitance for Author class/Inheitance for Author class/Author.cs
+++ Inheitance for Author class/Inheitance for Author class/Author.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Inheitance_for_Author_class
 {
-    internal class Class1
+    internal  class Author
     {
+        private string _FirstName;
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+
+                    throw new ArgumentNullException("First Name cant be empty");
+                else
+                { _FirstName = value; }
+
+            }
+
+        }
+        private string _LastName;
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("Last Name cant be emplty");
+                else { _LastName = value; }
+            }
+        }
+        public int id { get; set; }
+
+        public Author(string firstName, string lastName, int id)
+        {
+            this._FirstName= firstName;
+            this._LastName= lastName;
+            this.id = id;
+
+        }
+
+        List<Book> Authors = new List<Book>();
+        
+           
+
+           
     }
 }
--- Inheitance for Author class/Inheitance for Author class/Book.cs
+++ Inheitance for Author class/Inheitance for Author class/Book.cs
@@ -1,6 +1,20 @@
 ﻿namespace Inheitance_for_Author_class
 {
     internal class Book
+
     {
+        public string name { get; set; }
+        public int id { get; set; }
+
+        public  double price { get; set; }
+        public   int Republishyear { get; set; }
+        public Book(string name, int id, double price,int Republishyear)
+        {
+            this.name = name;
+            this.id = id;
+            this.price = price;
+            this.Republishyear = Republishyear;
+        }
+
     }
 }
