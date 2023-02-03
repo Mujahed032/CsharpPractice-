65308ee5e34d877bbc13a1c34543a441cbc622eb
Mujahed032 <mohdmujahed032@gmail.com>
2023-02-03 22:16:01 +0530

    Mul programme using Function
    Mul programme using Function

---
+++ Mul programme using Function/Mul programme using Function/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Mul_programme_using_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number between 1 to 10");
+            int number = int.Parse(Console.ReadLine());
+
+            int Mul = FunctiontogetMul(number);
+            Console.WriteLine($"Mul result is {Mul}");
+            Console.ReadLine();
+        }
+
+        private static int FunctiontogetMul(int number)
+        {
+            int mul = 1;
+            for(int i=1; i<=number;i++)
+            {
+                mul= mul*i;
+            }
+            return mul;
+        }
+    }
+}
