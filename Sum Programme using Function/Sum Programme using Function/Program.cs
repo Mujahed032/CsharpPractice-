e749ef4e8aff177689620c1009b5d37c316cf2f5
Mujahed032 <mohdmujahed032@gmail.com>
2023-02-03 22:07:04 +0530

    Sum programme using Function
    Sum programme using Function

---
+++ Sum Programme using Function/Sum Programme using Function/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Sum_Programme_using_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number between 1 to 10");
+            int number = int.Parse(Console.ReadLine());
+
+            int Sum = FunctiontogetSum(number);
+            Console.WriteLine($"your sum result is {Sum}");
+            Console.ReadLine();
+        }
+
+        private static int FunctiontogetSum(int number)
+        {
+            int sum = 0;
+            for(int i=1; i<=number; i++)
+            {
+                sum = sum + i;
+            }
+            return sum;
+        }
+    }
+}
