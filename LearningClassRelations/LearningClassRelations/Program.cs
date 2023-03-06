8fa046b2c471ac1cdec84c133a84c55858d9552c
Mujahed032 <mohdmujahed032@gmail.com>
2023-03-06 07:03:29 -0600

    Push Console App project for practicing relations
    Push Console App project for practicing relations

---
+++ LearningClassRelations/LearningClassRelations/Program.cs
@@ -0,0 +1,54 @@
+﻿using LearningClassRelations;
+using Microsoft.EntityFrameworkCore;
+
+var _context = new DataContext();
+//GetPhones("Ahmad");
+void GetPhones(string userName)
+{ 
+   
+    var users = _context.users.Include(p => p.phones).ToList();
+    foreach (var user in users)
+    {
+        if (user.Name == "Khizar")
+        {
+            foreach (var item in user.phones)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+      
+    }
+}
+
+//GetUsers("Samsung S20");
+
+void GetUsers(string phoneName)
+{
+    
+}
+
+GetAllPhonesByCategory("Android");
+
+void GetAllPhonesByCategory(string categoryName)
+{
+  //var phones = _context.phones.Where(p => p.Category.PhoneType == categoryName).Include(ph => ph.Category).ToList();
+
+    var phones = _context.phones.Include(ph => ph.Category).ToList();
+
+    foreach(var phone in phones)
+    {
+        if(phone.Category.PhoneType == categoryName)
+        {
+            Console.WriteLine(phone.Name);
+        }
+    }
+}
+
+GetAllUsersByCategory("IOS");
+
+void GetAllUsersByCategory(string v)
+{
+    throw new NotImplementedException();
+}
+
---
+++ LearningClassRelations/LearningClassRelations/User.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace LearningClassRelations
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Phone> phones { get; set; }
+    }
+}
