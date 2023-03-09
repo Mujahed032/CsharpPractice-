e417a8058b6e7d9b4984f74bb7f8267f0580615a
Mujahed032 <mohdmujahed032@gmail.com>
2023-03-09 05:44:55 -0600

    Add data via EF core code
    Add data via EF core code

--- LearningClassRelationsUsingConsole/LearningClassRelationsUsingConsole/Program.cs
+++ LearningClassRelationsUsingConsole/LearningClassRelationsUsingConsole/Program.cs
@@ -1,47 +1,36 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using LearningClassRelationsUsingConsole;
 
-namespace LearningClassRelationsUsingConsole
+InsertUser();
+void InsertUser()
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            void InsertUSer()
-            {
-                var course = new Course();
-                course.CourseName = "Physics";
-                course.CourseName = "Maths";
-                
-                var course2 = new Course();
-                course2.CourseName = "Chemistry";
-
-
-                var Teacher = new Teacher();
-                Teacher.Name = "Sami"; 
-                Teacher.Courses = new List<Course>();
-                Teacher.Courses.Add(course);
-               
-                Teacher.Name = "Rafi";
-                Teacher.Courses = new List<Course>();
-                Teacher.Courses.Add(course2);
+    var CoursePhysics = new Course() { CourseName = "physics" };
+    var CourseMaths = new Course() { CourseName = "Maths" };
+    var CourseEnglish = new Course() { CourseName = "English" };
+    var CourseSocial = new Course() { CourseName = "Social" };
+    var CourseHindi = new Course() { CourseName = "Hindi" };
+    var CourseTelugu = new Course() { CourseName = "Telugu" };
+    var CourseChemistry = new Course() { CourseName = "Chemistry" };
 
 
 
+    var TeacherKhizar = new Teacher() { Name = "Khizar", Courses = new List<Course>() { CoursePhysics } };
+    var TeacherAhmed = new Teacher() { Name = "Ahmed", Courses = new List<Course>() { CourseMaths } };
+    var TeacherAhsan = new Teacher() { Name = "Ahsan", Courses = new List<Course>() { CourseEnglish, CourseChemistry } };
+    var TeacherAzhar = new Teacher() { Name = "Azhar", Courses = new List<Course>() { CourseSocial, CourseHindi, CourseTelugu } };
 
 
+    var StudentRafi = new Student() { Name = "Rafi", courses = new List<Course>() { CoursePhysics } };
+    var StudentZaki = new Student() { Name = "Zaki", courses = new List<Course>() { CourseMaths, CourseEnglish, CourseChemistry } };
+    var StudentAzeem = new Student() { Name = "Azeem", courses = new List<Course>() { CourseHindi, CourseChemistry, CourseEnglish } };
+    var StudentShafi = new Student() { Name = "Shafi", courses = new List<Course>() { CourseTelugu, CourseSocial,CoursePhysics } };
+    var StudentSameer = new Student() { Name = "Sameer", courses = new List<Course>() { CourseMaths, CourseHindi, CourseEnglish } };
 
 
+    var _context = new DataContext();
+    _context.Add(StudentRafi);
+    _context.AddRange(new List<Student>() {StudentZaki, StudentAzeem, StudentShafi, StudentSameer});
+    _context.AddRange(new List<Teacher>() { TeacherKhizar, TeacherAhmed, TeacherAhsan, TeacherAzhar });
+    _context.SaveChanges();
 
-
-                Teacher.Name = "zaki";
-                
-
-
-                var student = new Student();
-                student.Name = "Ahsan";
-                student.Name = "khizar";
-                student.Name = "Ahmed";
-            }
-        }
-    }
 }
