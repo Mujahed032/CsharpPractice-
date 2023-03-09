b004dc667e45ca3c63e51f49b10a36eef3606fc5
Mujahed032 <mohdmujahed032@gmail.com>
2023-03-09 20:56:16 +0530

    Added Methods
    Added Methods

--- LearningClassRelationsUsingConsole/LearningClassRelationsUsingConsole/Program.cs
+++ LearningClassRelationsUsingConsole/LearningClassRelationsUsingConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using LearningClassRelationsUsingConsole;
+using Microsoft.EntityFrameworkCore;
 
+var _context = new DataContext();
 InsertUser();
 void InsertUser()
 {
@@ -23,14 +25,84 @@ void InsertUser()
     var StudentRafi = new Student() { Name = "Rafi", courses = new List<Course>() { CoursePhysics } };
     var StudentZaki = new Student() { Name = "Zaki", courses = new List<Course>() { CourseMaths, CourseEnglish, CourseChemistry } };
     var StudentAzeem = new Student() { Name = "Azeem", courses = new List<Course>() { CourseHindi, CourseChemistry, CourseEnglish } };
-    var StudentShafi = new Student() { Name = "Shafi", courses = new List<Course>() { CourseTelugu, CourseSocial,CoursePhysics } };
+    var StudentShafi = new Student() { Name = "Shafi", courses = new List<Course>() { CourseTelugu, CourseSocial, CoursePhysics } };
     var StudentSameer = new Student() { Name = "Sameer", courses = new List<Course>() { CourseMaths, CourseHindi, CourseEnglish } };
 
 
-    var _context = new DataContext();
+
     _context.Add(StudentRafi);
-    _context.AddRange(new List<Student>() {StudentZaki, StudentAzeem, StudentShafi, StudentSameer});
+    _context.AddRange(new List<Student>() { StudentZaki, StudentAzeem, StudentShafi, StudentSameer });
     _context.AddRange(new List<Teacher>() { TeacherKhizar, TeacherAhmed, TeacherAhsan, TeacherAzhar });
     _context.SaveChanges();
+
+    
+    GetAllStudentsByCourseName("Physics");
+    GetTeacher("physics");
+    GetAllCoursesByStudentName("Sameer");
+    GetAllTeachersByStudentName("zaki");
+    GetAllCoursesByTeacherName("Azhar");
+    GetAllStudentsByTeacherNAme("Ahmed");
+
+    void GetAllStudentsByCourseName(string CourseName)
+    {
+        var students = _context.courses.Include(c => c.students).ToList();
+        foreach (var student in students)
+        {
+            if(student.CourseName == CourseName)
+            {
+                Console.WriteLine(student.CourseName);
+            }
+        }
+        
+    }
+
+
+
+    
+    void GetTeacher(string CourseName)
+    {
+        var Teachers = _context.teachers.
+
+    }
+
+
+
+    void GetAllCoursesByStudentName(string StudentName)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+    
+    void GetAllTeachersByStudentName(string studentName)
+    {
+
+        throw new NotImplementedException();
+    }
+
+
+
+    
+    void GetAllCoursesByTeacherName(string TeacherName)
+    {
+        var courses = _context.teachers.Include(t => t.Courses).ToList();
+        foreach (var course in courses)
+        {
+            if (course.Name == TeacherName) ;
+            {
+                Console.WriteLine(course.Name);
+            }
+        }
+    }
+
+
+
+   
+    void GetAllStudentsByTeacherName(string TeacherName)
+    {
+        throw new NotImplementedException();
+    }
+
 
 }
