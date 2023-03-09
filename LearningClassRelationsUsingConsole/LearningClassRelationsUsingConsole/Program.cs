38d780486c369a4d0ee3294e927092649021b932
Mujahed032 <mohdmujahed032@gmail.com>
2023-03-09 16:20:08 +0530

    Added Insert
    Added Insert

--- LearningClassRelationsUsingConsole/LearningClassRelationsUsingConsole/Program.cs
+++ LearningClassRelationsUsingConsole/LearningClassRelationsUsingConsole/Program.cs
@@ -8,7 +8,39 @@ namespace LearningClassRelationsUsingConsole
         {
             void InsertUSer()
             {
+                var course = new Course();
+                course.CourseName = "Physics";
+                course.CourseName = "Maths";
                 
+                var course2 = new Course();
+                course2.CourseName = "Chemistry";
+
+
+                var Teacher = new Teacher();
+                Teacher.Name = "Sami"; 
+                Teacher.Courses = new List<Course>();
+                Teacher.Courses.Add(course);
+               
+                Teacher.Name = "Rafi";
+                Teacher.Courses = new List<Course>();
+                Teacher.Courses.Add(course2);
+
+
+
+
+
+
+
+
+
+                Teacher.Name = "zaki";
+                
+
+
+                var student = new Student();
+                student.Name = "Ahsan";
+                student.Name = "khizar";
+                student.Name = "Ahmed";
             }
         }
     }
