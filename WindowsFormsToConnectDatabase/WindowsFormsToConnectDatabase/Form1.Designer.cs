69c37396198a0456d5c9fc805756c79f944e07d9
Mujahed032 <mohdmujahed032@gmail.com>
2023-03-29 00:23:08 +0530

    WindowsForm
    WindowsForm

---
+++ WindowsFormsToConnectDatabase/WindowsFormsToConnectDatabase/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsToConnectDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TbFirstName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TbSecondName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(191, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // TbFirstName
+            // 
+            this.TbFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbFirstName.Location = new System.Drawing.Point(420, 62);
+            this.TbFirstName.Name = "TbFirstName";
+            this.TbFirstName.Size = new System.Drawing.Size(194, 39);
+            this.TbFirstName.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(288, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 67);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TbSecondName
+            // 
+            this.TbSecondName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbSecondName.Location = new System.Drawing.Point(420, 126);
+            this.TbSecondName.Name = "TbSecondName";
+            this.TbSecondName.Size = new System.Drawing.Size(194, 39);
+            this.TbSecondName.TabIndex = 3;
+            this.TbSecondName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(191, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Second Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TbSecondName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TbFirstName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TbFirstName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TbSecondName;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
